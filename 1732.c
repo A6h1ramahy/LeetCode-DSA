304e00eade164c70774a14a25eb170a8a4205a77
Abhirama H Y <abhiramhy836@gmail.com>
2026-06-19 09:27:43 +0530

    Find the Highest Altitude

---
+++ 1732.c
@@ -0,0 +1,12 @@
+int largestAltitude(int* gain, int gainSize) {
+    int cur = 0,result = 0;
+    for(int i=0; i<gainSize; i++)
+    {
+        cur = cur+gain[i];
+        if(cur>result)
+        {
+            result = cur;
+        }
+    }
+    return result;
+}
