db1a16987f63838e4048780fe3ea99e075dfa7d1
Abhirama H Y <abhiramhy836@gmail.com>
2026-07-11 11:01:43 +0530

    Number of 1 Bits

---
+++ 191.c
@@ -0,0 +1,12 @@
+int hammingWeight(int n) {
+    int result = 0;
+    while(n!=0)
+    {
+        if(n%2!=0)
+        {
+            result++;
+        }
+        n = n/2;
+    }
+    return result;
+}
