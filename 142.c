7a18ce7b03144cbbf69bb6a20b08aec2df956cba
Abhirama H Y <abhiramhy836@gmail.com>
2026-08-21 07:03:55 +0530

    Linked List Cycle II

---
+++ 142.c
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    if(!head)
+    {
+        return NULL;
+    }
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    while(fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(fast==slow)
+        {
+            break;
+        }
+    }
+    if(!fast || !fast->next)
+    {
+        return NULL;
+    }
+    slow = head;
+    while(slow!=fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return fast;
+}
