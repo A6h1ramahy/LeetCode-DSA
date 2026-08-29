e57d44c5b171186f1ff76cb661d5f6f8b1bc17bc
Abhirama H Y <abhiramhy836@gmail.com>
2026-08-30 00:01:15 +0530

    Time: 0 ms (100%), Space: 9.2 MB (40.42%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.c
@@ -0,0 +1,13 @@
+int addDigits(int num) {
+    while(!(num<=9))
+    {
+        int a=0;
+        while(num!=0)
+        {
+            a += num%10;
+            num /= 10;
+        }
+        num = a;
+    }
+    return num;
+}
