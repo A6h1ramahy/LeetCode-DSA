4b6472ab9a188cdf9a1dfc16797110b56bb1e03d
Abhirama H Y <abhiramhy836@gmail.com>
2026-08-29 09:03:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (65.27%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.c
+++ 0389-find-the-difference/0389-find-the-difference.c
@@ -1,23 +1,19 @@
 char findTheDifference(char* s, char* t) {
     int nums[26] = {0};
-    int i=0;
-    while(s[i]!='\0')
-    {
-        nums[s[i]-'a']++;
+    int i = 0;
+    while (s[i] != '\0') {
+        nums[s[i] - 'a']++;
         i++;
     }
-    i=0;
-    while(t[i]!='\0')
-    {
-        nums[t[i]-'a']--;
+    i = 0;
+    while (t[i] != '\0') {
+        nums[t[i] - 'a']--;
         i++;
     }
-    for(i=0; i<26; i++)
-    {
-        if(nums[i]==-1)
-        {
+    for (i = 0; i < 26; i++) {
+        if (nums[i] == -1) {
             break;
         }
     }
-    return (char)(i+'a');
+    return (char)(i + 'a');
 }
