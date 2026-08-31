f650ba05e6ca197c599e79a4fdff704a19a6ae60
Abhirama H Y <abhiramhy836@gmail.com>
2026-08-31 22:48:48 +0530

    Time: 3 ms (17.52%), Space: 15.7 MB (74.51%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.c
+++ 0338-counting-bits/0338-counting-bits.c
@@ -2,13 +2,21 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* countBits(int n, int* returnSize) {
-    int *result = calloc(n + 1, sizeof(int));
-    *returnSize = n + 1;
-
-    for(int i = 1; i <= n; i++)
+    int *result = calloc(n+1,sizeof(int));
+    *returnSize = 0;
+    while((*returnSize)<=n)
     {
-        result[i] = result[i >> 1] + (i & 1);
+        int count = 0;
+        int i = (*returnSize);
+        while(i!=0)
+        {
+            if(i%2!=0)
+            {
+                count++;
+            }
+            i /= 2;
+        }
+        result[(*returnSize)++] = count;
     }
-
     return result;
 }
